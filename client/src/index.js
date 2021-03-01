0c85d839ed1d382f302dcf1605d0f163da7d9b4a
Michelle Nguyen <nguyenmichelle@google.com>
2021-03-01 02:07:56 -0800

    remove localhost

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: '/graphql'
 });
 
 ReactDOM.render(<ApolloProvider client={client}>
