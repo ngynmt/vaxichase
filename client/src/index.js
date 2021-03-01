71d5b2b1abd83c0a6212e1d5c5deff39b9d0fe12
Michelle Nguyen <nguyenmichelle@google.com>
2021-03-01 09:09:15 -0800

    update api

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: '/.netlify/functions/graphql'
 });
 
 ReactDOM.render(<ApolloProvider client={client}>
